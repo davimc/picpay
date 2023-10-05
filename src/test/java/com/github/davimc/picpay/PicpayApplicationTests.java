fb2ad2ec674030d4c029d25ddc977d22922480bc
davimc <davimatosc@hotmail.com>
2023-10-05 09:58:21 -0300

    Project created

---
+++ src/test/java/com/github/davimc/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.davimc.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
