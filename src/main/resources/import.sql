faa6ab4adee2dfa0473d981d9b73c5450d1d4879
davimc <davimatosc@hotmail.com>
2023-10-06 21:17:37 -0300

    fix: discriminator

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO TB_USER (first_name, last_name, person_type, email, cpf,password) VALUES ('Bob', 'Finger', 'N','bob@gmail.com', '98410106078','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO TB_USER (first_name, last_name, person_type, email, cpf,password) VALUES ('Ana', 'Frost', 'N','ana@gmail.com', '32298763003','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO TB_USER (legal_name, trade_name, person_type, email, cnpj, password) VALUES ('anapoli corp inc', 'Anapoli', 'L','anapoli@corp.com', '98894721000197','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO TB_USER (first_name, last_name, person_type, email, cpf,password) VALUES ('Bob', 'Finger', 'NATURAL','bob@gmail.com', '98410106078','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO TB_USER (first_name, last_name, person_type, email, cpf,password) VALUES ('Ana', 'Frost', 'NATURAL','ana@gmail.com', '32298763003','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO TB_USER (legal_name, trade_name, person_type, email, cnpj, password) VALUES ('anapoli corp inc', 'Anapoli', 'LEGAL','anapoli@corp.com', '98894721000197','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 --
 INSERT INTO tb_role (authority) VALUES ('ROLE_USUARIO');
 INSERT INTO tb_role (authority) VALUES ('ROLE_LOJISTA');
