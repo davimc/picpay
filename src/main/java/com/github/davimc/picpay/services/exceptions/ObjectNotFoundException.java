ed2dad8213459a56910ae0dd957af85a2553ad70
davimc <davimatosc@hotmail.com>
2023-10-11 09:37:32 -0300

    Object not found

--- src/main/java/com/github/davimc/picpay/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/github/davimc/picpay/services/exceptions/ObjectNotFoundException.java
@@ -4,8 +4,10 @@ public class ObjectNotFoundException extends RuntimeException{
     public ObjectNotFoundException(String message) {
         super(message);
     }
-
-    public ObjectNotFoundException(Long id,Class clazz) {
-        super("ID: " + id + " não encontrado para a classe " + clazz.getSimpleName());
+    public ObjectNotFoundException(Long id, Class<?> clazz) {
+        super("Id: " + id + " não encontrado para a classe: " + clazz.getSimpleName());
+    }
+    public ObjectNotFoundException(String identificador, Class<?> clazz) {
+        super("Identificador: " + identificador + " não encontrado para a classe: " + clazz.getSimpleName());
     }
 }
