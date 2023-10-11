3b8db70617e4a55e9897f7c866b3acea431db75e
davimc <davimatosc@hotmail.com>
2023-10-11 09:38:36 -0300

    fix

--- src/main/java/com/github/davimc/picpay/repositories/PersonRepository.java
+++ src/main/java/com/github/davimc/picpay/repositories/PersonRepository.java
@@ -2,7 +2,9 @@ package com.github.davimc.picpay.repositories;
 
 import com.github.davimc.picpay.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
