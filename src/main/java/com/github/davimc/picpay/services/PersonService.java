8b5d5749f3671d8e47afe8a15a2fe4bdb7d0a651
davimc <davimatosc@hotmail.com>
2023-10-15 08:33:13 -0300

    Transactional

--- src/main/java/com/github/davimc/picpay/services/PersonService.java
+++ src/main/java/com/github/davimc/picpay/services/PersonService.java
@@ -8,27 +8,32 @@ import com.github.davimc.picpay.services.exceptions.ObjectNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
     private final PersonRepository repository;
 
+
     public PersonService(PersonRepository repository) {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public Page<PersonDTO> findAll(Pageable pageable) {
         return repository.findAll(pageable).map(PersonDTO::new);
     }
-
+    @Transactional(readOnly = true)
     protected Person find(Long id) {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id, Person.class));
 
     }
+    @Transactional(readOnly = true)
     public PersonDTO findByID(Long id) {
         return new PersonDTO(find(id));
     }
 
+    @Transactional
     public PersonDTO create(PersonNewDTO dto) {
         Person obj = dto.toEntity();
         obj = repository.save(obj);
