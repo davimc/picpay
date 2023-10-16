c3a6b2fc21bea768c74d32e57ab46aa40e25c66f
davimc <davimatosc@hotmail.com>
2023-10-16 08:23:59 -0300

    update: validation, personId

--- src/main/java/com/github/davimc/picpay/DTO/RegisterDTO.java
+++ src/main/java/com/github/davimc/picpay/DTO/RegisterDTO.java
@@ -1,6 +1,16 @@
 package com.github.davimc.picpay.DTO;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
-public record RegisterDTO(String login, String password, Long personId, List<String> roles) {
+public record RegisterDTO(
+        @NotNull(message = "Login is required")
+        String login,
+        @NotNull(message = "Password is required")
+        String password,
+        @NotNull(message = "person is required")
+        Long personId,
+        @NotNull(message = "Perfil is required")
+        List<String> roles) {
 }
