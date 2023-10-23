f489638d99a59eb26db5c2e5ec3ff72f2c7aef6a
davimc <davimatosc@hotmail.com>
2023-10-22 21:58:19 -0300

    update: rest template

--- src/main/java/com/github/davimc/picpay/config/AppConfig.java
+++ src/main/java/com/github/davimc/picpay/config/AppConfig.java
@@ -3,11 +3,17 @@ package com.github.davimc.picpay.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
