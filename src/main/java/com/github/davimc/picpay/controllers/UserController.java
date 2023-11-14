4fe13f5d7fa765f63ccd622185018f3f59bc319c
davimc <davimatosc@hotmail.com>
2023-11-14 20:00:10 -0300

    update: crossorigin to 5173

--- src/main/java/com/github/davimc/picpay/controllers/UserController.java
+++ src/main/java/com/github/davimc/picpay/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins="http://localhost:5173")
 public class UserController {
 
     private final UserService service;
